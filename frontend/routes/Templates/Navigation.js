f0cc8ffb38d3ac3e38c98676a52c3ea2e1a817fb
PaVM <pavboxdev@gmail.com>
2016-10-11 02:02:22 +0300

    created Navigation Drawer

--- frontend/routes/Templates/Navigation.js
+++ frontend/routes/Templates/Navigation.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { IndexLink, Link } from 'react-router';
 
-import Drawer from 'material-ui/Drawer';
-import MenuItem from 'material-ui/MenuItem';
+import Videogame  from 'material-ui/svg-icons/hardware/videogame-asset';
+import Email      from 'material-ui/svg-icons/communication/email';
+import Security   from 'material-ui/svg-icons/hardware/security';
+import NavMenu    from 'material-ui/svg-icons/navigation/menu';
+import Archive    from 'material-ui/svg-icons/content/archive';
+import Person     from 'material-ui/svg-icons/social/person';
+import Cake       from 'material-ui/svg-icons/social/cake';
 import IconButton from 'material-ui/IconButton';
-import NavMenu  from 'material-ui/svg-icons/navigation/menu';
-import Divider from 'material-ui/Divider';
+import SubHeader  from 'material-ui/Subheader';
+import MenuItem   from 'material-ui/MenuItem';
+import Divider    from 'material-ui/Divider';
+import Drawer     from 'material-ui/Drawer';
 
  class Navigation extends React.Component {
   constructor(props) {
@@ -18,7 +25,25 @@ import Divider from 'material-ui/Divider';
 
   render() {
     const styleLink = {
-      textDecoration: 'none'
+      textDecoration: 'none',
+      fontWeight: 'normal'
+    }
+
+    const styleNavHead = {
+      backgroundColor: '#aea',
+      marginBottom: '4px',
+      textAlign: 'center',
+      lineHeight: '96px',
+      display: 'block'
+    }
+
+    const styleDivider = {
+      marginTop: '4px',
+      marginBottom: '4px'
+    }
+
+    const styleIconDisabled = {
+      opacity: '0.6'
     }
 
     return (
@@ -29,15 +54,44 @@ import Divider from 'material-ui/Divider';
 
         <Drawer
           open={this.state.open}
-          openSecondary={true}
-          width={300}
+          width={350}
           docked={false}
           onRequestChange={this.closeDrawer} >
-            <MenuItem>pavboxdev</MenuItem>
+
+            <span style={styleNavHead}>Pavboxdev</span>
+
             <IndexLink to='/' style={styleLink} onTouchTap={this.toggleDrawer}>
-              <MenuItem>Главная страница</MenuItem></IndexLink>
+              <MenuItem
+                primaryText='Обо мне'
+                leftIcon={<Person />}/></IndexLink>
+
+              <MenuItem
+                primaryText='Мои проекты'
+                leftIcon={<Cake style={styleIconDisabled} />}
+                disabled={true} />
+
             <Link to='/settings' style={styleLink} onTouchTap={this.toggleDrawer}>
-              <MenuItem>Настройки</MenuItem></Link>
+              <MenuItem
+                primaryText='Контакты'
+                leftIcon={<Email />} /></Link>
+
+            <Divider style={styleDivider}/>
+            <SubHeader>Больше, чем проекты</SubHeader>
+
+              <MenuItem
+                primaryText='Библиотека компонентов'
+                leftIcon={<Archive style={styleIconDisabled} />}
+                disabled={true} />
+
+              <MenuItem
+                primaryText='Информационная безопасность'
+                leftIcon={<Security style={styleIconDisabled} />}
+                disabled={true} />
+
+              <MenuItem
+                primaryText='Разработка игр'
+                leftIcon={<Videogame style={styleIconDisabled} />}
+                disabled={true} />
         </Drawer>
       </div>
     );
