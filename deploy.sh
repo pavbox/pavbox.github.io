91e52c4d05b3d8b222e66233d62c2c7572827791
pavbox <pavboxdev@gmail.com>
2017-10-12 00:40:07 +0300

    add deployment shell script

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+
+# once deployment script;
+# remove repository, clone actual from master
+# install dependencies and build project
+# daemonize website
+
+# @required pm2 utility
+# @required git, npm and nodejs
+# @recommended nodejs 8.6.0+
+
+rm -rf * .*
+ls -al
+
+git clone https://github.com/pavbox/pavbox.github.io.git .
+
+npm install && npm run build
+
+pm2 delete pavboxcom
+pm2 start backend/server.js --name "pavboxcom"
+
