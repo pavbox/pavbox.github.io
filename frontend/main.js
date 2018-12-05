cd7bc2a4e68db87d3d33e5519163f3fd9509c169
pavbox <pavboxdev@gmail.com>
2018-12-06 01:15:11 +0300

    remove route on iq test

--- frontend/main.js
+++ frontend/main.js
@@ -1,6 +1,5 @@
 
 import Application from './application/Application';
-import IQChecker from './application/IQChecker';
 
 // import images
 import './assets/img/favicon.ico';
@@ -10,6 +9,6 @@ import './assets/img/man.svg';
 import './assets/img/star.svg';
 import './assets/img/stone.svg';
 
-const application = (window.location.pathname.includes('iq')) ? new IQChecker() : new Application();
+const application = new Application();
 
 application.init();
