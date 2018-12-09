51a394d7fc46f271c0e515caac572e198b546f23
pavbox <pavboxdev@gmail.com>
2018-12-09 16:11:40 +0300

    removed iq test resources from js import

--- frontend/main.js
+++ frontend/main.js
@@ -3,11 +3,6 @@ import Application from './application/Application';
 
 // import images
 import './assets/img/favicon.ico';
-import './assets/img/baby.svg';
-import './assets/img/cow.svg';
-import './assets/img/man.svg';
-import './assets/img/star.svg';
-import './assets/img/stone.svg';
 
 const application = new Application();
 
