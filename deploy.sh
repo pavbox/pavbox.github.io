648177781d0006b944beabedfff5df339e9f9ee0
pavbox <pavboxdev@gmail.com>
2022-10-18 22:56:15 +0300

    updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -15,5 +15,5 @@ git clone https://github.com/pavbox/pavbox.github.io.git .
 npm install && npm run build
 
 pm2 delete pavboxcom
-pm2 start backend/server.js --name "pavboxcom"
+pm2 start src/backend/server.js --name "pavboxcom"
 
